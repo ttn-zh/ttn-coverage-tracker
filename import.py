b414736abedb19338ddb39b1add7123375c551ab
Gonzalo Casas <gonzalo.casas@local.ch>
2016-01-18 05:17:58 +0100

    Visualize receiving gateway and RSSI on the map

--- import.py
+++ import.py
@@ -114,7 +114,47 @@ def geojson_feature(data_point):
     point = geojson.Point((lon, lat))
     props = dict(hash=data_point_hash, gateway_eui=data_point['gateway_eui'], time=data_point['time'], rssi=data_point['rssi'], snr=data_point['snr'], data_rate=data_point['datarate'])
 
+    # Add styling features https://help.github.com/articles/mapping-geojson-files-on-github/
+    props['marker-symbol'] = get_marker_symbol(data_point)
+    props['marker-color'] = get_marker_color(data_point)
+
     return geojson.Feature(geometry=point, properties=props) 
+
+
+gateway_marker_symbols = {}
+
+def get_marker_symbol(data_point):
+    eui = data_point['gateway_eui']
+    if eui in gateway_marker_symbols:
+        return gateway_marker_symbols[eui]
+
+    symbol = chr(len(gateway_marker_symbols) + 97)
+    gateway_marker_symbols[eui] = symbol
+
+    return symbol
+
+def get_marker_color(data_point):
+    try:
+        rssi = float(data_point['rssi'])
+    except:
+        return color
+
+    color = '#7d8182'  # default to grey
+
+    if rssi < 0 and rssi >= -70:
+        color = '#005a32'
+    if rssi < -70:
+        color = '#238443'
+    if rssi < -90:
+        color = '#41ab5d'
+    if rssi < -100:
+        color = '#78c679'
+    if rssi < -110:
+        color = '#addd8e'
+    if rssi < -115:
+        color = '#d9f0a3'
+
+    return color
 
 def dms2decimal(degrees, minutes, seconds):
     op = operator.__add__ if degrees >= 0 else operator.__sub__
